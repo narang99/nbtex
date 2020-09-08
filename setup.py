ff6bfb81d84a687d6fa6e2eb37295a412aeafe69
Hariom Narang <hariom2711@gmail.com>
2020-09-08 20:35:10 +0530

    1.0.5 setup change

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="nbtex",
-    version="1.0.3",
+    version="1.0.5",
     author="Hariom Narang",
     author_email="hariom2711@gmail.com",
     packages=find_packages(),
